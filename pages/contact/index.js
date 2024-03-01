9e6013cc19256f03356589fde445afd7d0e0909b
abhi kumar <9984907494abhi@gmail.com>
2024-03-01 18:29:36 +0530

    changing gap bettwen input

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -138,7 +138,7 @@ const Contact = () => {
               initial="hidden"
               animate="show"
               exit="hidden"
-              className="flex-1 flex flex-col gap-6 w-full mx-0 lg:mx-auto"
+              className="flex-1 flex flex-col gap-6 w-full mx-auto"
             >
               {/* {input group} */}
               <div className="flex gap-x-6 w-full">
